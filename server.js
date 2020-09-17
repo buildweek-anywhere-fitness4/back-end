679416f930f1b7d6da676db74b8661ee5988368e
Michael Sheets <michaelsheets@MacBook-Pro.local>
2020-09-17 03:02:36 -0500

    updated server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use("/api/clients", authenticator, clientRouter);
+server.use("/api/clients", clientRouter);
 server.use("/api/auth", clientAuthRouter);
 
 server.get("/", (req, res) => {
