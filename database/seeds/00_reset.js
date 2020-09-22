7c52f69a447db352e98f6c321018597020853802
Michael Sheets <michaelsheets@MacBook-Pro.local>
2020-09-21 20:33:37 -0500

    updating seeds to hopefully work

--- database/seeds/00_reset.js
+++ database/seeds/00_reset.js
@@ -1,6 +1,13 @@
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex("class").truncate();
-  await knex("instructor").truncate();
-  await knex("client").truncate();
+  const production = process.env.NODE_ENV === "production";
+  if (production) {
+    await knex.raw("TRUNCATE TABLE class CASCADE");
+    await knex.raw("TRUNCATE TABLE instructor CASCADE");
+    await knex.raw("TRUNCATE TABLE client CASCADE");
+  } else {
+    await knex("class").truncate();
+    await knex("instructor").truncate();
+    await knex("client").truncate();
+  }
 };
